6a097a7373dc35e6d5d81725a1b1a5c2d1755581
Aryan0312 <aryan26zzz@gmail.com>
2026-02-24 16:17:01 +0530

    db change

--- src/db/db.sql
+++ src/db/db.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict D8vd5YXgtBzeLby9sZxBhusZrODtSKxnk3bXgi69plbX5YP4dYEv4E7n7z0xtkr
+\restrict UD03j4gSbVkMEBBqzT5Dkmsd0RFMvgTdIcFsdb65Pp9FunuEXcPalTfcbgGZTUa
 
 -- Dumped from database version 18.1 (Ubuntu 18.1-1.pgdg24.04+2)
 -- Dumped by pg_dump version 18.1 (Ubuntu 18.1-1.pgdg24.04+2)
 
--- Started on 2026-02-23 17:08:20 IST
+-- Started on 2026-02-24 16:16:45 IST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -147,6 +147,21 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 COPY public.notes (id, user_id, title, content, created_at, updated_at) FROM stdin;
 2	1	first note bym e	hello buddy light weight	2026-02-23 15:39:49.362446	2026-02-23 15:39:49.362446
 3	1	first note bym e	hello buddy light weight	2026-02-23 15:56:49.723538	2026-02-23 15:56:49.723538
+6	2	sdcbdsiuvb	hii my name is khalynayak	2026-02-24 16:00:24.501764	2026-02-24 16:09:17.592553
+7	2	ausbdc vuisd	adbviu	2026-02-24 16:09:29.139138	2026-02-24 16:09:29.139138
+8	2	sdkcubsd	dcbuisd b	2026-02-24 16:09:32.924391	2026-02-24 16:09:32.924391
+9	2	sd cvsdb b	sdbviu sd	2026-02-24 16:09:36.185151	2026-02-24 16:09:36.185151
+10	2	sdvbiubvuib	vdibv ui	2026-02-24 16:09:39.474987	2026-02-24 16:09:39.474987
+11	2	dbv uisdoqsdewv u	vidu bvuib	2026-02-24 16:09:43.814544	2026-02-24 16:09:43.814544
+12	2	sdbv uios	dfvbsdiu b	2026-02-24 16:09:48.272445	2026-02-24 16:09:48.272445
+13	2	sdbvui dbiub	f iuewrbuieb	2026-02-24 16:09:51.436073	2026-02-24 16:09:51.436073
+14	2	sdbv uibvui	fweuiv uib	2026-02-24 16:09:54.658655	2026-02-24 16:09:54.658655
+15	2	sdbv uifbui	vbweui dvfb	2026-02-24 16:09:57.97696	2026-02-24 16:09:57.97696
+16	2	sd biufb	d bvfiduvui	2026-02-24 16:10:04.863488	2026-02-24 16:10:04.863488
+17	2	sdbv uifbiu	fvwub uiwqb	2026-02-24 16:10:08.315219	2026-02-24 16:10:08.315219
+18	2	aryan shrivastav	shrivastav aryan	2026-02-24 16:10:18.730973	2026-02-24 16:10:18.730973
+19	8	hii my name is yagnik	kjbsdv ubbv ytdj	2026-02-24 16:11:59.668936	2026-02-24 16:12:06.668311
+20	8	mnb yukkWre stj f	trhd fkuk	2026-02-24 16:12:12.235182	2026-02-24 16:12:12.235182
 \.
 
 
@@ -157,7 +172,7 @@ COPY public.notes (id, user_id, title, content, created_at, updated_at) FROM std
 --
 
 COPY public.user_sessions (sid, sess, expire) FROM stdin;
-GAH4XmA40yZjTIEttj0Ph6skC2SCzoQD	{"cookie":{"originalMaxAge":86400000,"expires":"2026-02-24T10:24:53.938Z","secure":false,"httpOnly":true,"path":"/"},"userId":1}	2026-02-24 15:59:43
+epqfL8DpgloRoTaePY5gFe8-JvrypBwj	{"cookie":{"originalMaxAge":86400000,"expires":"2026-02-25T06:09:27.173Z","secure":false,"httpOnly":true,"path":"/"},"userId":1,"name":"aryan","email":"aryan26zzz@gmail.com"}	2026-02-25 11:39:43
 \.
 
 
@@ -169,6 +184,13 @@ GAH4XmA40yZjTIEttj0Ph6skC2SCzoQD	{"cookie":{"originalMaxAge":86400000,"expires":
 
 COPY public.users (id, name, email, password, created_at) FROM stdin;
 1	aryan	aryan26zzz@gmail.com	$2b$10$aAHTWucdWuByyQ/cmo67v.oTl0SlQCtKDPOW9mUO4lEZzlIJZc9vm	2026-02-23 12:41:48.423316
+2	Aryan Shrivastav	aryan@gmail.com	$2b$10$KPlPBV6NZK2ujVNnOlDunuQdUjddsiqbdS8sjL4jeUXLZfAvvHD1S	2026-02-24 11:42:32.304017
+3	aryan	aryan1@gmail.com	$2b$10$AZWbvNAe9pHhz8YzISdXmuehKfx456l2EuGuAWOVjbDB3sd8V/91e	2026-02-24 11:54:58.24304
+4	aryan	aryannn@gmail.com	$2b$10$0Tgm6r5PJXdUOsHTL4YZIudUIB0AbhlEYVe4NXelRqEWn74fS.pfq	2026-02-24 12:30:56.392814
+5	aryan	aryannnnn@gmail.com	$2b$10$HGGUAJ3arl99d8mts8UWB.Vy788jCNPe31jUq9quYTwBf7Bpto4QO	2026-02-24 12:33:07.545329
+6	123	123@gmail.com	$2b$10$3Kt9tmap7MJdbwHcnOt9DOsFS3fx4z7PZIrMjexvAkEoZPtGBhavm	2026-02-24 15:08:16.546221
+7	aryan	arya@gmail.com	$2b$10$AZUn3HJlSeodc4gx803YNelGsfQMzBKXAUdazMkyvt4HGfNjCacum	2026-02-24 15:14:23.824712
+8	aksh	aksh@gmail.com	$2b$10$mIY09KSaH2Y.YedOYWrwQeeaxCClVM2.lnUqhnnycUzg15X7d.Niq	2026-02-24 16:11:42.119648
 \.
 
 
@@ -178,7 +200,7 @@ COPY public.users (id, name, email, password, created_at) FROM stdin;
 -- Name: notes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.notes_id_seq', 3, true);
+SELECT pg_catalog.setval('public.notes_id_seq', 20, true);
 
 
 --
@@ -187,7 +209,7 @@ SELECT pg_catalog.setval('public.notes_id_seq', 3, true);
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+SELECT pg_catalog.setval('public.users_id_seq', 8, true);
 
 
 --
@@ -243,11 +265,11 @@ ALTER TABLE ONLY public.notes
     ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
 
--- Completed on 2026-02-23 17:08:20 IST
+-- Completed on 2026-02-24 16:16:45 IST
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict D8vd5YXgtBzeLby9sZxBhusZrODtSKxnk3bXgi69plbX5YP4dYEv4E7n7z0xtkr
+\unrestrict UD03j4gSbVkMEBBqzT5Dkmsd0RFMvgTdIcFsdb65Pp9FunuEXcPalTfcbgGZTUa
 
